d55090914232958c21a59f952a2ec24810bb528c
uczen <uczen@stacja9.egzamin.local>
2022-03-24 11:19:18 +0100

    php klasa wydarzenie

--- event.php
+++ event.php
@@ -1,5 +1,48 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
 
+$conn = new mysqli($servername, $username, $password, "kalendarz");
 
+$sql = "INSERT INTO kalendarz (, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+class Wydarzenie{
+
+    public $data;
+    public $godzina1;
+    public $godzina2;
+    public $tytul;
+    public $opis;
+
+    public function UstawDate($data){
+        $this->data = $data;
+    }
+//=========godz_start=========
+    public function UstawGodzineStart($godzina1){
+        $this->godzina1 = $godzina1;
+    }
+    public function UstawGodzineKoniec($godzina2){
+        $this->godzina2 = $godzina2;
+    }
+//=========godz_koniec========
+    public function UstawTytul($tytul){
+        $this->tytul = $tytul;
+    }
+    public function UstawOpis($opis){
+        $this->opis = $opis;
+    }
+
+}
+
+?>
 
 
 <!DOCTYPE html>
@@ -10,22 +53,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form>
         <label for="data">Data</label>
-        <input type="date" value=""> </br></br>
+        <input type="date" name="data" id=""> </br></br>
 
-        <label for="godzina">Godzina</label>
-        <input type="time" value=""></br></br>
+        <label for="godzina1">Od godziny</label>
+        <input type="time" name="godzina1" id="">
+        <label for="godzina2">do godziny</label>
+        <input type="time" name="godzina2" id="">
+
+</br></br>
 
         <label for="tytul">Tytuł</label>
-        <input type="text" value=""> </br></br>
+        <input type="text" name="tytul" id=""> </br></br>
 
         <label for="opis">Notka</label>
-        <input type="text" value=""> </br></br>
+        <input type="text" name="opis" id=""> </br></br>
 
         
-    </form>
-    
+    </form>  
 </body>
 </html> 
