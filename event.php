6d4e04ef46f370a054412a4161c04142b394c04b
uczen <uczen@stacja9.egzamin.local>
2022-03-24 10:22:20 +0100

    formularz dodawania wydarzenia

---
+++ event.php
@@ -0,0 +1,31 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <label for="data">Data</label>
+        <input type="date" value=""> </br></br>
+
+        <label for="godzina">Godzina</label>
+        <input type="time" value=""></br></br>
+
+        <label for="tytul">Tytuł</label>
+        <input type="text" value=""> </br></br>
+
+        <label for="opis">Notka</label>
+        <input type="text" value=""> </br></br>
+
+        
+    </form>
+    
+</body>
+</html> 
