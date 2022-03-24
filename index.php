7b445b6c5ad0797435b132f6371fbfcbb74fa4e6
Your Name <you@example.com>
2022-03-24 10:48:40 +0100

    index view

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-    
-?>
+<html>
+    <header>
+        <link rel="stylesheet" href="static/index.css">
+    </header>
+    <body>
+        <div class="header">
+        </div>
+    </body>
+</html>
